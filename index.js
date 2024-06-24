4f7182f4b739912984c18dcf46d3794c9194ff7a
aufarabul <aufarabul@gmail.com>
2024-06-24 15:52:03 +0700

    deploy

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = require("./route");
 require("dotenv").config();
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(
   fileUpload({
     useTempFiles: true,
+    tempFileDir: process.env.NODE_ENV == "development" ? "./tmp" : "/tmp",
   })
 );
+
 app.use(express.static("public"));
 
 app.use("/api", router);
